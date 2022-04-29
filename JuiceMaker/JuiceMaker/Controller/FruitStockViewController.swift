d1d11a7bf2b20fc34dcea2f65124f968834e2872
bard <schy2503@naver.com>
2022-04-29 20:27:52 +0900

    feat: 과일재고를 관리할 화면 뷰컨 생성

---
+++ JuiceMaker/JuiceMaker/Controller/FruitStockViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FruitStockViewController.swift
+//  JuiceMaker
+//
+//  Created by 김동용 on 2022/04/29.
+//
+
+import UIKit
+
+class FruitStockViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
