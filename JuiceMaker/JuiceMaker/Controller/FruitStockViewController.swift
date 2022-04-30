04e0a970cf9c3503f6cfe0088736d9e5b6b09ed4
bard <schy2503@naver.com>
2022-04-30 18:20:04 +0900

    feat: FruitStockViewController 각 버튼 연결

--- JuiceMaker/JuiceMaker/Controller/FruitStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitStockViewController.swift
@@ -13,6 +13,24 @@ class FruitStockViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var changeStrawberryStock: UILabel!
+    @IBOutlet weak var changeBananaStock: UILabel!
+    @IBOutlet weak var changePineappleStock: UILabel!
+    @IBOutlet weak var changKiwiStock: UILabel!
+    @IBOutlet weak var changeMangoStock: UILabel!
     
+    @IBAction func changeStrawberryStockButton(_ sender: Any) {
+    }
     
+    @IBAction func changeBananaStockButton(_ sender: Any) {
+    }
+    
+    @IBAction func changePineappleStockButton(_ sender: Any) {
+    }
+    
+    @IBAction func changeKiwiStockButton(_ sender: Any) {
+    }
+    
+    @IBAction func changeMangoStockButton(_ sender: Any) {
+    }
 }
