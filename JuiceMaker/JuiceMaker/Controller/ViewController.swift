4148b997e2a1d85ac419d6353d92a34b6c8801fa
bard <schy2503@naver.com>
2022-04-30 17:40:58 +0900

    feat: 메인 뷰 컨트롤러 버튼 연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -13,6 +13,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBOutlet weak var strawberryStockButton: UILabel!
+    @IBOutlet weak var bananaStockButton: UILabel!
+    @IBOutlet weak var pineappleStockButton: UILabel!
+    @IBOutlet weak var kiwiStockButton: UILabel!
+    @IBOutlet weak var mangoStockButton: UILabel!
+    
+    @IBAction func makeStrawberryBananaJuiceButton(_ sender: Any) {
+    }
+    
+    @IBAction func makeMangoKiwiJuiceButton(_ sender: Any) {
+    }
+    
+    @IBAction func makeStrawberryJuiceButton(_ sender: Any) {
+    }
+    
+    @IBAction func makeBananJuiceButton(_ sender: Any) {
+    }
+    
+    @IBAction func makePineappleJuiceButton(_ sender: Any) {
+    }
+    
+    @IBAction func makeKiwiJuiceButton(_ sender: Any) {
+    }
+    
+    @IBAction func makeMangoJuiceButton(_ sender: Any) {
+    }
+    
     
 }
 
